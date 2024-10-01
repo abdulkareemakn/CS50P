0fe41e31ffcfb9368ebd935d7e836df44b8a56d8
abdulkareemakn <abdulkareemakn@proton.me>
2024-10-01 13:19:27 +0500

    PS2

---
+++ src/extensions/extensions.py
@@ -0,0 +1,22 @@
+filetype = input("File Name: ")
+
+filetype = filetype.casefold()
+filetype = filetype.strip()
+
+filetype, extension = filetype.split(".")
+
+match extension:
+    case "gif":
+        print("image/gif")
+    case "jpg" | "jpeg":
+        print("image/jpeg")
+    case "png":
+        print("image/png")
+    case "pdf":
+        print("application/pdf")
+    case "txt":
+        print("text/plain")
+    case "zip":
+        print("application/zip")
+    case _:
+        print("application/octet-stream")
--- src/interpreter/interpreter.py
+++ src/interpreter/interpreter.py
@@ -1,1 +1,20 @@
-input("Expression: ")
+exp = input("Expression: ")
+
+x, y, z = exp.split()
+
+x = float(x)
+z = float(z)
+
+if y == "+":
+    print(x + z)
+
+elif y == "-":
+    print(x - z)
+
+elif y == "*":
+    print(x * z)
+
+elif y == "/":
+    print(x / z)
+else:
+    print("Invalid Expression")
