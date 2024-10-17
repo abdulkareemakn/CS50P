36491f3b355696520d7efbb375a2b10452bcfe79
abdulkareemakn <abdulkareemakn@proton.me>
2024-10-17 20:26:46 +0500

    Done

---
+++ src/taqueria/taqueria.py
@@ -0,0 +1,27 @@
+felipe = {
+    "Baja Taco": 4.25,
+    "Burrito": 7.50,
+    "Bowl": 8.50,
+    "Nachos": 11.00,
+    "Quesadilla": 8.50,
+    "Super Burrito": 8.50,
+    "Super Quesadilla": 9.50,
+    "Taco": 3.00,
+    "Tortilla Salad": 8.00,
+}
+
+sum = 0
+
+while True:
+    try:
+        item = input("Item: ")
+        item = item.title()
+        try:
+            sum += felipe.get(item)
+            print(f"Total: ${sum:.2f}")
+        except (KeyError, TypeError):
+            pass
+    except EOFError:
+        break
+
+print()
